8ddca53eb6206c0472e2227cd7d53a46647d856d
Guilherme Conti <guibascnti@gmail.com>
2017-05-05 17:29:17 -0500

    5 Frames for a shot (rasp fps bug need fix)

--- server/controllers/photo.js
+++ server/controllers/photo.js
@@ -1,41 +1,55 @@
 var cv = require('opencv');
 const webcam = new cv.VideoCapture(0);
 
+var counter = 0;
+
 exports.normalPhoto = (req, res) => {
 
     try {
 
-        webcam.read((err, webcamImage) => {
+	webcam.read((err, webcamImage) => {
+		webcam.read((err, webcamImage) => {
+			webcam.read((err, webcamImage) => {
+				webcam.read((err, webcamImage) => {
+					webcam.read((err, _webcamImage) => {
+					
+					            if (err) throw err;
+					
+					            if (_webcamImage.size()[0] > 0 && _webcamImage.size()[1] > 0){
+					
+					                //console.log(_webcamImage.size());
+							counter++;
+					                _webcamImage.save('./image/room' + counter + '.jpg');
+					                res.status(200).sendFile(process.cwd() + '/image/room' + counter + '.jpg');
+									
+					
+					                //window.show(webcamImage);
+					
+					                /*webcamImage.detectObject(cv.FACE_CASCADE, {}, function(err, faces) {
+					
+					                    if (err) throw err;
+					
+					                    for (var i=0; i<faces.length; i++){
+					
+					                        var x = faces[i]
+					                        webcamImage.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
+					
+					                    }
+					                
+					                    webcamImage.save('./out' + counter + '.jpg');
+					
+					                });*/
+					
+					            }
+					
+				        });
 
-            if (err) throw err;
+				});
+			});
+		});
+	});
 
-            if (webcamImage.size()[0] > 0 && webcamImage.size()[1] > 0){
-
-                //console.log(webcamImage.size());
-                webcamImage.save('./image/room.jpg');
-                res.status(200).sendFile(process.cwd() + '/image/room.jpg');
-
-                //window.show(webcamImage);
-
-                /*webcamImage.detectObject(cv.FACE_CASCADE, {}, function(err, faces) {
-
-                    if (err) throw err;
-
-                    for (var i=0; i<faces.length; i++){
-
-                        var x = faces[i]
-                        webcamImage.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
-
-                    }
-                
-                    webcamImage.save('./out' + counter + '.jpg');
-
-                });*/
-
-            }
-
-        });
-
+        
     } catch (err){
 
         console.log(err);
